a5faded4f49a71cb3c74ccf374fae6a9eedea5e2
evaristo-va <69491362+evaristo-va@users.noreply.github.com>
2025-09-30 19:47:27 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ import pandas as pd
 from collections import deque
 from utils.price_simulator import simulate_prices
 from utils.market_book_simulator import simulate_market_book
-from utils.market_maker import get_bid_ask
+from utils.market_maker import get_bid_ask, get_bid_ask_avellaneda
 from utils.execute_trades import execute_trades
 from utils.pnl import compute_unrealized_pnl
 from utils.estimate_sigma import sigma_ewma
-from typing import Optional, Dict
+from typing import Optional, Dict, Tuple
 
 def run_simulation(
     T: int = 390, 
@@ -17,8 +17,10 @@ def run_simulation(
     spread: float = 0.001,
     cost_per_share: float = 0.0, 
     use_avellaneda: bool = False, 
+    k: float = 20,
+    gamma: float = 0.0001,
     prices: Optional[pd.Series] = None
-) -> pd.DataFrame:
+) -> Tuple[pd.DataFrame,list]:
     
     inventory = deque()
     realized_pnl = 0
@@ -29,6 +31,7 @@ def run_simulation(
     mid_price_history = []
     mtm_pnl_history = []
     realized_pnl_history = []
+    order_book_history = []
 
     if prices is None:
         prices = simulate_prices(T=T, S0=S0, sigma=sigma)
@@ -50,7 +53,7 @@ def run_simulation(
         inventory_qty = sum(qty for qty,_ in inventory)
 
         if use_avellaneda:
-            mm_bid, mm_ask = get_bid_ask_avellaneda(mid_price, inventory_qty, t/T, 1.0)
+            mm_bid, mm_ask = get_bid_ask_avellaneda(mid_price, inventory_qty, t/T, sigma, gamma, k)
         else:
             mm_bid, mm_ask = get_bid_ask(mid_price,inventory_qty,sigma=sigma)
 
@@ -65,6 +68,15 @@ def run_simulation(
         mtm_pnl_history.append(realized_pnl + compute_unrealized_pnl(inventory, mid_price,cost_per_share=cost_per_share))
         realized_pnl_history.append(realized_pnl)
 
+        order_book_history.append({
+            "bids": bids,
+            "asks": asks,
+            "mid": mid_price,
+            "mm_bid": mm_bid,
+            "mm_ask": mm_ask
+        })
+        
+
     # create DataFrame for plotting
     df = pd.DataFrame({
         'Price': prices,
@@ -76,4 +88,4 @@ def run_simulation(
         'Realized_PnL': realized_pnl_history
     })
 
-    return df
+    return df, order_book_history
